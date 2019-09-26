b737597a0a63280b34a7fb91bdc1824f384d2522
k-okina <okinakahiro@gmail.com>
2019-09-26 17:47:12 +0900

    注釈追加

--- src/views/Example2.vue
+++ src/views/Example2.vue
@@ -12,16 +12,17 @@ import { createComponent } from "@vue/composition-api";
 import { useMouse } from "@/hooks/useMouse";
 
 export default createComponent({
-  setup () {
+  setup() {
     const { x, y } = useMouse();
 
     return {
       x,
       y,
       setDummyPos: () => {
-        // これはreadonlyなcomputedの為エラーがでる
-        x.value = Math.random();
-        y.value = Math.random();
+        // これは以下のプルリクがマージされたらcompile errorになる
+        // https://github.com/vuejs/composition-api/pull/145
+        // x.value = Math.random();
+        // y.value = Math.random();
       }
     };
   }
