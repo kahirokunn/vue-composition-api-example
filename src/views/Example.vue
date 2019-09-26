4cc6d5857e98a9fe067f39ea313f39de9bec079a
k-okina <okinakahiro@gmail.com>
2019-09-26 17:09:33 +0900

    minimum vue-composition-api example

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="example"></div>
+  <div>
+    <h2>messageOne: {{ state.messageOne }}</h2>
+    <h2>messageTwo: {{ messageTwo }}</h2>
+    <button @click="updateMessaageOne">set Random num to messaageOne</button>
+    <button @click="updateMessaageTwo">set Random num to messaageTwo</button>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { createComponent, reactive, ref } from "@vue/composition-api";
+
+export default createComponent({
+  setup() {
+    const state = reactive({
+      messageOne: "Hello"
+    });
+    const messageTwo = ref("こんにちは");
+
+    return {
+      state,
+      messageTwo,
+      updateMessaageOne: () => {
+        state.messageOne = Math.random().toString();
+      },
+      updateMessaageTwo: () => {
+        messageTwo.value = Math.random().toString();
+      }
+    };
+  }
+});
 </script>
